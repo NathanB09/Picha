b22752b370793fc78b21d43783aa061ca5bdf896
NathanB09 <nathanbell09@gmail.com>
2019-01-22 18:36:21 +0000

    event listeners added. edited getLables, saveTag

--- src/index.js
+++ src/index.js
@@ -1,13 +1,57 @@
 const photoForm = document.querySelector('#add-photo-form')
 const titleInput = document.querySelector('#title')
 const urlInput = document.querySelector('#url')
+const cardHold = document.querySelector('.card-holder')
 
 const state = {
   pichas: []
 }
 
-function renderPhoto() {
+function renderPhoto(photo) {
+  const aTag = document.createElement('a')
+  const imgTag = document.createElement('img')
 
+  imgTag.src = photo.url
+  imgTag.className = 'photo-card'
+
+  aTag.href = '#popup2'
+
+  aTag.append(imgTag)
+
+  cardHold.append(aTag)
+
+  aTag.addEventListener('click', () => {
+    const windowPic = document.querySelector('.window-photo')
+    const tagsContainer = document.querySelector('.tags-container')
+    tagsContainer.innerHTML = ''
+    windowPic.src = photo.url
+    state.pichas.find(pic => pic.id === photo.id).tags
+      .forEach(tag => {
+        const tagBtn = document.createElement('button')
+        const tagSpan = document.createElement('span')
+
+        tagBtn.className = 'tag-button'
+        tagSpan.innerText = tag.description
+
+        tagBtn.append(tagSpan)
+        tagsContainer.append(tagBtn)
+      })
+  })
+}
+
+function renderPhotos(photos) {
+  photos.forEach(photo => renderPhoto(photo))
+}
+
+getPhotos().then(data => {
+  // console.log(data)
+  state.pichas = data
+  renderPhotos(state.pichas)
+})
+
+function updatePage() {
+  cardHold.innerHTML = ''
+  renderPhotos(state.pichas)
 }
 
 photoForm.addEventListener('submit', (event) => {
@@ -18,9 +62,20 @@ photoForm.addEventListener('submit', (event) => {
     url: urlInput.value
   }
 
-  state.pichas.push(newPhoto)
+  savePicha(newPhoto).then(photo => {
+    state.pichas.push(photo)
+    updatePage()
 
-  // savePicha()
+    getLabels(photo.url).then(data => {
+      data.responses[0].labelAnnotations.forEach(tag => {
+        saveTag(tag).then(tagData => {
+          saveRelevance(tagData.id, photo.id)
+        })
+      })
+    })
+
+  })
 
   photoForm.reset()
+
 })
